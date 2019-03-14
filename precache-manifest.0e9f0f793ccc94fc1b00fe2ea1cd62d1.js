481232f855c1b3c9dde0478709014dd8deea66ce
belintex <belintex4@gmail.com>
2019-03-15 01:30:49 +0700

    Updates

---
+++ precache-manifest.0e9f0f793ccc94fc1b00fe2ea1cd62d1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc911bccafe83200c5e3",
+    "url": "/belintex/buildReact/static/css/main.ab041fe8.chunk.css"
+  },
+  {
+    "revision": "bc911bccafe83200c5e3",
+    "url": "/belintex/buildReact/static/js/main.4505a8e0.chunk.js"
+  },
+  {
+    "revision": "2582375115441ed74682",
+    "url": "/belintex/buildReact/static/js/runtime~main.47290f8e.js"
+  },
+  {
+    "revision": "305f18b81789a2c95751",
+    "url": "/belintex/buildReact/static/js/2.fc1f4cc4.chunk.js"
+  },
+  {
+    "revision": "0c9c79076698a0781ea548cbd18ad467",
+    "url": "/belintex/buildReact/index.html"
+  }
+];
